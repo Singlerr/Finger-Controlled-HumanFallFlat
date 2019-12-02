fd54d23c100f6a515aa3342afb1bcb229d5cb276
Singlerr <singlerr@naver.com>
2019-12-02 23:07:46 +0900

    1차 완성

---
+++ ArduinoCon/ArduinoCon/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ArduinoCon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.TextBox();
+            this.right = new System.Windows.Forms.TextBox();
+            this.status = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lstat = new System.Windows.Forms.Label();
+            this.rstat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(12, 12);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(100, 25);
+            this.left.TabIndex = 1;
+            // 
+            // right
+            // 
+            this.right.Location = new System.Drawing.Point(122, 12);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(100, 25);
+            this.right.TabIndex = 2;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(12, 40);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(22, 15);
+            this.status.TabIndex = 3;
+            this.status.Text = "...";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // lstat
+            // 
+            this.lstat.AutoSize = true;
+            this.lstat.Location = new System.Drawing.Point(12, 84);
+            this.lstat.Name = "lstat";
+            this.lstat.Size = new System.Drawing.Size(36, 15);
+            this.lstat.TabIndex = 4;
+            this.lstat.Text = "Left:";
+            // 
+            // rstat
+            // 
+            this.rstat.AutoSize = true;
+            this.rstat.Location = new System.Drawing.Point(12, 101);
+            this.rstat.Name = "rstat";
+            this.rstat.Size = new System.Drawing.Size(45, 15);
+            this.rstat.TabIndex = 5;
+            this.rstat.Text = "Right:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 125);
+            this.Controls.Add(this.rstat);
+            this.Controls.Add(this.lstat);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Arduino";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox left;
+        private System.Windows.Forms.TextBox right;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lstat;
+        private System.Windows.Forms.Label rstat;
+    }
+}
+
---
+++ ArduinoCon/ArduinoCon/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.IO.Ports;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+namespace ArduinoCon
+{
+
+    public partial class Form1 : Form
+    {
+        
+        double anglePrev = 0;
+        double time, timePrev = 0;
+        private SerialPort leftPort, rightPort;
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+
+
+      
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static double Current()
+        {
+            DateTime Jan1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan javaSpan = DateTime.UtcNow - Jan1970;
+            return javaSpan.TotalMilliseconds;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Left_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void PerformLeftClick()
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+        }
+        private void PerformRightClick()
+        {
+            mouse_event(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            leftPort = new SerialPort();
+            rightPort = new SerialPort();
+            leftPort.PortName = left.Text;
+            rightPort.PortName = right.Text;
+            leftPort.BaudRate = 115200;
+            rightPort.BaudRate = 115200;
+            leftPort.DataReceived += DataReceivedL;
+            rightPort.DataReceived += DataReceivedR;
+            leftPort.Open();
+            rightPort.Open();
+            status.Text = "Connected";
+        }
+        public static int Map(int value, int fromSource, int toSource, int fromTarget, int toTarget)
+        {
+            return (value - fromSource) / (toSource - fromSource) * (toTarget - fromTarget) + fromTarget;
+        }
+        private void DataReceivedR(object sender, SerialDataReceivedEventArgs e)
+        {
+            double angle = 0;
+            bool flag = double.TryParse(rightPort.ReadExisting(), out angle);
+            if (flag)
+            {
+                rstat.Text = "Right: " + angle;
+                int k = 0;
+                if (angle < 0)
+                {
+                    k = 0;
+                }
+                else
+                if (angle > 180)
+                {
+                    k = 180;
+                }
+                else
+                {
+                    k = (int)angle;
+                }
+                if(k > 160)
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                }
+                else if(30 < k)
+                {
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                    timer2.Interval = Map(k, 0, 160, 31, 100);
+                    timer2.Start();
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                }
+
+            }
+        }
+        private double Abs(double x)
+        {
+            if(x < 0)
+            {
+                return -x;
+            }
+            else
+            {
+                return x;
+            }
+            
+        }
+       
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            leftPort = new SerialPort();
+            rightPort = new SerialPort();
+            leftPort.PortName = left.Text;
+            rightPort.PortName = right.Text;
+            leftPort.BaudRate = 115200;
+            rightPort.BaudRate = 115200;
+            leftPort.DataReceived += DataReceivedL;
+            rightPort.DataReceived += DataReceivedR;
+            leftPort.Open();
+            rightPort.Open();
+            status.Text = "Connected";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mouse_event(MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+            timer2.Stop();
+        }
+
+        private void Calculate(double angle)
+        {
+            time = Current();
+            double k = (angle - anglePrev) / (time - timePrev);
+            anglePrev = angle;
+            timePrev = time;
+            if(Abs(k)> 10)
+            {
+                
+            }
+        }
+        private void DataReceivedL(object sender, SerialDataReceivedEventArgs e)
+        {
+            double angle = 0;
+            bool flag = double.TryParse(leftPort.ReadExisting(), out angle);
+            if (flag)
+            {
+                lstat.Text = "Left: " + angle;
+                int k = 0;
+                if (angle < 0)
+                {
+                    k = 0;
+                }
+                else
+                if (angle > 180)
+                {
+                    k = 180;
+                }
+                else
+                {
+                    k = (int)angle;
+                }
+                if (k > 160)
+                {
+                    mouse_event(MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                }
+                else if (30 < k)
+                {
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                    mouse_event(MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                    timer2.Interval = Map(k, 0, 160, 31, 100);
+                    timer2.Start();
+                }
+                else
+                {
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                }
+
+            }
+        }
+    }
+}
